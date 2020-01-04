c242f97d313c61014b6aa9502bacf23a318c4723
hrg921 <hrg921@gmail.com>
2020-01-04 15:34:16 +0900

    feat: initialize Application

---
+++ src/main/kotlin/com/geonho/decisioner/DecisionerApplication.kt
@@ -0,0 +1,11 @@
+package com.geonho.decisioner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DecisionerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DecisionerApplication>(*args)
+}
---
+++ src/test/kotlin/com/geonho/decisioner/DecisionerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.geonho.decisioner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DecisionerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
